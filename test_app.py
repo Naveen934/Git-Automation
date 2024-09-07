8540e2f41ce7941f68a8692a61fa649632dd0589
Naveen J <naveenlenin934.com>
2024-09-07 21:59:14 +0530

    updated3

--- test_app.py
+++ test_app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 import pytest
 from streamlit.testing import TestClient
@@ -10,15 +9,3 @@ def test_home():
     
     assert response.status_code == 200
     assert b"Hello World!" in response.content
-=======
-import pytest
-from streamlit.testing.v1 import AppTest
-from app import home
-
-def test_home():
-    at = AppTest.from_function(home)
-    at.run()
-    
-    # Check if the title is present
-    assert at.title[0].value == "Hello World!"
->>>>>>> ffe8315 (updated2)
